9ede2931dadfe1d7b1c6aeeb4c8a144ea5f6bcd1
Joshuah Boardman <joshuahboardman@gmail.com>
2023-09-22 09:57:04 -0500

    Update: added a bunch of random stuff to help learn actix web

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 mod routes;
 
-use routes::test::{test, index};
+use routes::test::{test, test_JSON, test_from_path, send_post};
 
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Might need to add move to the closure
     HttpServer::new(|| {
         App::new()
            .service(test)
-           .service(index)
+           .service(test_JSON)
+           .service(test_from_path)
+           .service(send_post)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
--- src/routes/test.rs
+++ src/routes/test.rs
@@ -1,7 +1,37 @@
-use actix_web::{web, get, HttpResponse, Responder, Result};
+use actix_web::{web, get, HttpResponse, Responder, Result, web::Json, post};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct TestData {
+    test_string: String,
+}
 
 #[get("/test")]
 pub async fn test() -> impl Responder {
     HttpResponse::Ok().body("testing123")
+}
+
+#[get("/test/JSON")]
+pub async fn test_JSON() -> Json<String> {
+    Json("testing in JSON".to_string())
+}
+
+#[get("/test/fromPath/{test_string}")]
+pub async fn test_from_path(test_string: web::Path<TestData>) -> Json<String> {
+   let test_string = test_string.into_inner().test_string;
+   Json(format!("This is the test string: {}", test_string)) 
+}
+
+#[derive(Serialize, Deserialize)]
+struct TestBlogPost {
+    id: u32,
+    title: String, 
+    body: String,
+    tags: Vec<String>
+}
+
+#[post("/test/send_post")]
+pub async fn send_post(body: Json<TestBlogPost>) -> Json<String> {
+   Json(body.into_inner().body) 
 }
 
