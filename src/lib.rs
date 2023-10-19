1b3e46d913aed524bc248eceb818a13a62b1d4d9
Joshuah Boardman <joshuahboardman@Joshuahs-Laptop.attlocal.net>
2023-10-18 20:13:19 -0500

    Update: the lib run function is no longer async and now recieves a tcp_listener and a pg_pool which allows the run function to complete instead of running indefinitely

--- src/lib.rs
+++ src/lib.rs
@@ -2,35 +2,24 @@ pub mod configuration;
 pub mod model;
 mod routes;
 
-use configuration::get_configuration;
-
-use routes::books::{get_book_by_id, get_books, post_book};
+use routes::books::{ get_book_by_id, get_books, post_book };
 use routes::health_check::health_check;
+use actix_web::dev::Server;
+use actix_web::{web::Data, App, HttpServer};
+use sqlx::PgPool;
+use std::net::TcpListener;
 
-use actix_web::{web, App, HttpServer};
-
-//#[actix_web::main]
-pub async fn run() -> std::io::Result<()> {
-    let configuration_settings = get_configuration();
-    let database_settings = &configuration_settings.database;
-
-    let database_url = database_settings.get_database_url();
-
-    print!("{}", database_url);
-    let pool = sqlx::postgres::PgPool::connect(&database_url)
-        .await
-        .expect("Failed to read configuration.");
-    let pool = web::Data::new(pool);
-
-    HttpServer::new(move || {
+pub fn run(listener: TcpListener, db_pool: Data<PgPool>) -> std::io::Result<Server> {
+    let server = HttpServer::new(move || {
         App::new()
-            .app_data(pool.clone())
+            .app_data(db_pool.clone())
             .service(health_check)
             .service(post_book)
             .service(get_book_by_id)
             .service(get_books)
     })
-    .listen(configuration_settings.get_tcp_listener())?
-    .run()
-    .await
+    .listen(listener)?
+    .run();
+
+    Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
+pub mod configuration;
+
 use digital_bookshelf_api::run;
+use configuration::get_configuration;
+use sqlx::PgPool;
+use actix_web::web::Data;
+
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
-    run().await
+    let configuration_settings = get_configuration();
+    let database_settings = &configuration_settings.database;
+    let database_url = database_settings.get_database_url();
+    let pool = PgPool::connect(&database_url)
+        .await
+        .expect("Failed to read configuration");
+    let pool = Data::new(pool);
+
+    run(configuration_settings.get_tcp_listener(), pool)?.await
 }
